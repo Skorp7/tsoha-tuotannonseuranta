15f8d600bee100107cf36cfb4bccf4bafc5b7e27
Sanna <sanna.korpi@live.fi>
2020-09-15 14:39:44 +0300

    kirjautumisen ja käyttäjän muokkauksen hahmottelua

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect
 import users, visits
+from db import db
+from flask import session
 
 @app.route("/")
 def index():
@@ -37,3 +39,28 @@ def register():
             return redirect("/")
         else:
             return render_template("error.html",message="Rekisteröinti ei onnistunut, kokeile toista käyttäjätunnusta.")
+
+@app.route("/admin", methods=["get"])
+def admin():
+    if request.method == "GET":
+        if users.user_status() == 1:
+            return render_template("admin.html")
+        else:
+            return render_template("error.html",message="Käyttäjän oikeudet eivät riitä tähän toimintoon.")
+        
+@app.route("/change_status", methods=["get","post"])
+def change_status():
+    if request.method == "GET":
+        user_list = users.user_list()
+        return render_template("change_status.html", user_list=user_list)
+    if request.method == "POST":
+        new_status = request.form["new_status"]
+        username = request.form["username"]
+        if users.update_status(username, new_status):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Statuksen vaihto epäonnistui.")
+
+@app.route("/production")
+def production():
+    return render_template("production.html")
--- users.py
+++ users.py
@@ -21,7 +21,7 @@ def logout():
 def register(username,password):
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (name,status,password,visible) VALUES (:name,1,:password, 1)"
+        sql = "INSERT INTO users (name,status,password,visible) VALUES (:name,0,:password, 1)"
         db.session.execute(sql, {"name":username,"password":hash_value})
         db.session.commit()
     except:
@@ -30,3 +30,29 @@ def register(username,password):
 
 def user_id():
     return session.get("user_id",0)
+
+def user_status():
+    id = session.get("user_id",0)
+    sql = "SELECT status FROM users WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    status = result.fetchone()[0]
+    return status
+
+def update_status(username, new_status):
+    try:
+        sql = "UPDATE users SET status=:status WHERE name=:name"
+        db.session.execute(sql, {"status":new_status,"name":username})
+        db.session.commit()
+        return True
+    except:
+        return False
+
+def user_list():
+    try:
+        sql = "SELECT name, status FROM users ORDER BY name"
+        result = db.session.execute(sql)
+        user_list = result.fetchall()
+        return user_list
+    except:
+        return None
+    
