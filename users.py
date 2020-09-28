8403eac2106ae6451317b4cc93d68a9715590b8c
Sanna <sanna.korpi@live.fi>
2020-09-28 19:09:01 +0300

    added seek user -function to ensure there is no user with that name when register, added register-field to layout

--- users.py
+++ users.py
@@ -5,24 +5,41 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 def login(username,password):
-    sql = "SELECT password, id, status FROM users WHERE name=:name"
-    result = db.session.execute(sql, {"name":username})
-    user = result.fetchone()
-    if user == None:
-        return False
-    else:
-        if check_password_hash(user[0],password):
-            session["user_id"] = user[1]
-            session["user_status"] = user[2]
-            return True
-        else:
+    try:
+        sql = "SELECT password, id, status FROM users WHERE name=:name"
+        result = db.session.execute(sql, {"name":username})
+        user = result.fetchone()
+        if user == None:
             return False
+        else:
+            if check_password_hash(user[0],password):
+                session["user_id"] = user[1]
+                session["user_status"] = user[2]
+                return True
+            else:
+                return False
+    except:
+        return False
+
+def seek(username):
+    try:
+        sql = "SELECT * FROM Users WHERE name ILIKE :name"
+        result = db.session.execute(sql, {"name":username})
+        user = result.fetchone()
+        if user == None:
+            return False
+        else:
+            return True
+    except:
+        return False
 
 def logout():
     del session["user_id"]
     del session["user_status"]
 
 def register(username,password):
+    if seek(username):
+        return False
     hash_value = generate_password_hash(password)
     try:
         sql = "INSERT INTO users (name,status,password,visible) VALUES (:name,0,:password, 1)"
@@ -44,10 +61,13 @@ def user_status():
 
 def user():
     usr_id = session.get("user_id",0)
-    sql = "SELECT id, name, status FROM users WHERE id=:id"
-    result = db.session.execute(sql, {"id":usr_id})
-    user_data = result.fetchone()
-    return user_data
+    try:
+        sql = "SELECT id, name, status FROM users WHERE id=:id"
+        result = db.session.execute(sql, {"id":usr_id})
+        user_data = result.fetchone()
+        return user_data
+    except:
+        return None
 
 def update_status(username, new_status):
     try:
