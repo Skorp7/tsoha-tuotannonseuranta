b26bad0c4faecec0e24d9e702649156bd603ede0
Sanna <sanna.korpi@live.fi>
2020-10-08 15:06:41 +0300

    piechart added to charts.html and code cleaning in users.py

--- orders.py
+++ orders.py
@@ -4,8 +4,11 @@ from flask import session
 
 def add(order_type_id, customer_id, delivery_date, clinic_id):
     try:
-        sql = "INSERT INTO orders (order_type_id, customer_id, clinic_id, delivery_date, time, in_progress) VALUES (:order_type_id, :customer_id, :clinic_id, :delivery_date, LOCALTIMESTAMP, 1) RETURNING id"
-        result = db.session.execute(sql, {"order_type_id":order_type_id, "customer_id":customer_id, "clinic_id":clinic_id, "delivery_date":delivery_date})
+        sql = "INSERT INTO orders (order_type_id, customer_id, clinic_id, delivery_date, time, in_progress) " \
+            "VALUES (:order_type_id, :customer_id, :clinic_id, :delivery_date, LOCALTIMESTAMP, 1) RETURNING id"
+        result = db.session.execute(
+            sql, {"order_type_id": order_type_id, "customer_id": customer_id, \
+                "clinic_id": clinic_id, "delivery_date": delivery_date})
         latest_id = result.fetchone()[0]
         db.session.commit()
         return latest_id
@@ -16,44 +19,46 @@ def add(order_type_id, customer_id, delivery_date, clinic_id):
 def add_order_type(product_type, main_materials):
     try:
         sql = "INSERT INTO order_types (product_type, main_materials) VALUES (:product_type, :main_materials)"
-        db.session.execute(sql, {"product_type":product_type,"main_materials":main_materials})
+        db.session.execute(
+            sql, {"product_type": product_type, "main_materials": main_materials})
         db.session.commit()
         return True
     except:
         return False
 
-def order_type_list():
-    try:
-        sql = "SELECT id, product_type, main_materials FROM order_types ORDER BY product_type"
-        result = db.session.execute(sql)
-        order_type_list = result.fetchall()
-        return order_type_list
-    except:
-        return None
 
-def list(date):
-    try:
-        sql = "SELECT O.id, O.order_type_id, OT.product_type, OT.main_materials, O.customer_id, C.name, O.clinic_id, CL.name, CL.city, O.delivery_date, O.time, O.in_progress FROM orders O LEFT JOIN customers C on C.id=O.customer_id LEFT JOIN order_types OT on OT.id=O.order_type_id LEFT JOIN clinics CL ON CL.id=O.clinic_id  WHERE O.delivery_date::text LIKE :date ORDER BY O.delivery_date"
-        result = db.session.execute(sql, {"date":date+"%"})
-        order_list = result.fetchall()
-        return order_list
-    except:
-        return None
+def order_type_list():
+    sql = "SELECT id, product_type, main_materials FROM order_types ORDER BY product_type"
+    result = db.session.execute(sql)
+    order_type_list = result.fetchall()
+    return order_type_list
+
+
+def order_list(date):
+    sql = "SELECT O.id, O.order_type_id, OT.product_type, OT.main_materials, O.customer_id, " \
+        "C.name, O.clinic_id, CL.name, CL.city, O.delivery_date, O.time, O.in_progress FROM orders O " \
+        "LEFT JOIN customers C on C.id=O.customer_id LEFT JOIN order_types OT on OT.id=O.order_type_id "\
+        "LEFT JOIN clinics CL ON CL.id=O.clinic_id  WHERE O.delivery_date::text LIKE :date ORDER BY O.delivery_date"
+    result = db.session.execute(sql, {"date": date+"%"})
+    order_list = result.fetchall()
+    return order_list
+
 
 def listAll():
-    try:
-        sql = "SELECT O.id, O.order_type_id, OT.product_type, OT.main_materials, O.customer_id, C.name, O.clinic_id, CL.name, CL.city, O.delivery_date, O.time, O.in_progress FROM orders O LEFT JOIN customers C on C.id=O.customer_id LEFT JOIN order_types OT on OT.id=O.order_type_id LEFT JOIN clinics CL ON CL.id=O.clinic_id ORDER BY O.id"
-        result = db.session.execute(sql)
-        order_list = result.fetchall()
-        return order_list
-    except:
-        return None
- 
+    sql = "SELECT O.id, O.order_type_id, OT.product_type, OT.main_materials, O.customer_id, C.name, " \
+        "O.clinic_id, CL.name, CL.city, O.delivery_date, O.time, O.in_progress FROM orders O LEFT JOIN " \
+        "customers C on C.id=O.customer_id LEFT JOIN order_types OT on OT.id=O.order_type_id LEFT JOIN " \
+        "clinics CL ON CL.id=O.clinic_id ORDER BY O.id"
+    result = db.session.execute(sql)
+    order_list = result.fetchall()
+    return order_list
+
 
 def update_delivery_date(new_datetime, order_id):
     try:
         sql = "UPDATE orders SET delivery_date=:delivery_date WHERE id=:id"
-        db.session.execute(sql, {"delivery_date":new_datetime,"id":order_id})
+        db.session.execute(
+            sql, {"delivery_date": new_datetime, "id": order_id})
         db.session.commit()
         return True
     except:
@@ -61,20 +66,29 @@ def update_delivery_date(new_datetime, order_id):
 
 # check out: inprogress = 0
 # check in: inprogress = 1
+
+
 def check_out_in(order_id, in_progress):
     try:
         sql = "UPDATE orders SET in_progress=:in_progress WHERE id=:id"
-        db.session.execute(sql, {"in_progress":in_progress,"id":order_id})
+        db.session.execute(sql, {"in_progress": in_progress, "id": order_id})
         db.session.commit()
         return True
     except:
         return False
 
+
 def order(order_id):
-    try:
-        sql = "SELECT * FROM orders WHERE id=:id"
-        result = db.session.execute(sql, {"id":order_id})
-        order_data = result.fetchall()
-        return order_data
-    except:
-        return None
+    sql = "SELECT * FROM orders WHERE id=:id"
+    result = db.session.execute(sql, {"id": order_id})
+    order_data = result.fetchall()
+    return order_data
+
+def percent_orders_list():
+    sql2 = "SELECT ot.product_type, count(*) from orders o left join order_types ot on o.order_type_id=ot.id group by ot.product_type"
+    sql = "SELECT ot.product_type, (count(*)::decimal/(select count(*) from orders))*100 AS percent " \
+        "from orders o left join order_types ot on o.order_type_id=ot.id group by ot.product_type"
+    result = db.session.execute(sql2)
+    percent_orders = result.fetchall()
+    percent_orders_not_tuples = [list(elem) for elem in percent_orders]
+    return percent_orders_not_tuples
--- users.py
+++ users.py
@@ -3,88 +3,81 @@ from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
-
-def login(username,password):
-    try:
-        sql = "SELECT password, id, status FROM users WHERE name ILIKE :name"
-        result = db.session.execute(sql, {"name":username})
-        user = result.fetchone()
-        if user == None:
-            return False
-        else:
-            if check_password_hash(user[0],password):
-                session["user_id"] = user[1]
-                session["user_status"] = user[2]
-                return True
-            else:
-                return False
-    except:
+def login(username, password):
+    sql = "SELECT password, id, status FROM users WHERE name ILIKE :name"
+    result = db.session.execute(sql, {"name": username})
+    user = result.fetchone()
+    if user == None:
         return False
+    else:
+        if check_password_hash(user[0], password):
+            session["user_id"] = user[1]
+            session["user_status"] = user[2]
+            return True
+        else:
+            return False
+
 
 def seek(username):
-    try:
-        sql = "SELECT * FROM Users WHERE name ILIKE :name"
-        result = db.session.execute(sql, {"name":username})
-        user = result.fetchone()
-        if user == None:
-            return False
-        else:
-            return True
-    except:
+    sql = "SELECT * FROM Users WHERE name ILIKE :name"
+    result = db.session.execute(sql, {"name": username})
+    user = result.fetchone()
+    if user == None:
         return False
+    else:
+        return True
+
 
 def logout():
     del session["user_id"]
     del session["user_status"]
 
-def register(username,password):
+
+def register(username, password):
     if seek(username):
         return False
     hash_value = generate_password_hash(password)
     try:
         sql = "INSERT INTO users (name,status,password,visible) VALUES (:name,0,:password, 1)"
-        db.session.execute(sql, {"name":username,"password":hash_value})
+        db.session.execute(sql, {"name": username, "password": hash_value})
         db.session.commit()
     except:
         return False
-    return login(username,password)
+    return login(username, password)
+
 
 def user_id():
-    return session.get("user_id",0)
+    return session.get("user_id", 0)
+
 
 def user_status():
-    usr_id = session.get("user_id",0)
+    usr_id = session.get("user_id", 0)
     sql = "SELECT status FROM users WHERE id=:id"
-    result = db.session.execute(sql, {"id":usr_id})
+    result = db.session.execute(sql, {"id": usr_id})
     status = result.fetchone()[0]
     return status
 
+
 def user():
-    usr_id = session.get("user_id",0)
-    try:
-        sql = "SELECT id, name, status FROM users WHERE id=:id"
-        result = db.session.execute(sql, {"id":usr_id})
-        user_data = result.fetchone()
-        return user_data
-    except:
-        return None
+    usr_id = session.get("user_id", 0)
+    sql = "SELECT id, name, status FROM users WHERE id=:id"
+    result = db.session.execute(sql, {"id": usr_id})
+    user_data = result.fetchone()
+    return user_data
+
 
 def update_status(username, new_status):
     try:
         sql = "UPDATE users SET status=:status WHERE name=:name"
-        db.session.execute(sql, {"status":new_status,"name":username})
+        db.session.execute(sql, {"status": new_status, "name": username})
         db.session.commit()
         return True
     except:
         return False
 
-def user_list():
-    try:
-        sql = "SELECT name, status FROM users ORDER BY name"
-        result = db.session.execute(sql)
-        user_list = result.fetchall()
-        return user_list
-    except:
-        return None
 
-   
+def user_list():
+    sql = "SELECT name, status FROM users ORDER BY name"
+    result = db.session.execute(sql)
+    user_list = result.fetchall()
+    return user_list
